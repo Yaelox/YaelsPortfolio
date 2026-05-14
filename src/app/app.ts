74f0fa88b487e4d6f1de803c77b431d7ce87e58d
Yael <yaelsilvestre64@gmail.com>
2026-05-14 17:40:40 -0600

    Page About y Item

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from "./shared/header/header";
 import { Footer } from "./shared/footer/footer";
 import { Portafolio } from "./pages/portafolio/portafolio";
+import { About } from "./pages/about/about";
+import { PortfolioItem } from "./pages/portfolio-item/portfolio-item";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, Portafolio],
+  imports: [RouterOutlet, Header, Footer, Portafolio, About, PortfolioItem],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/pages/portfolio-item/portfolio-item.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-item',
+  imports: [],
+  templateUrl: './portfolio-item.html',
+  styleUrl: './portfolio-item.css',
+})
+export class PortfolioItem {}
